3b4c07d85b1941688d411822da26122e1db30ee7
Jocelyn Chang <jrc300@gmail.com>
2023-06-12 16:43:51 -0700

    added link to Eats Google Sheet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ export default function mainPage() {
             <ul>
                 <li><Link href="/upload_ocr_text">Input the EasyOCR results array</Link></li>
                 <li><Link href="/upload_menu_snapshot">Upload a menu snapshot to search</Link></li>
+                <li><a href="https://docs.google.com/spreadsheets/d/18rHSAq_OmQDYqke-SvkN08AFkBvKTgJRKpnP7u09raE/edit#gid=1831747662" target="_blank">Go To Eats Google Sheet</a></li>
             </ul>
         </div>
     )
